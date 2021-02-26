d3c74ee2f86cde61838469463312c80690480498
Spaulding <cs237217@ohio.edu>
2021-02-26 11:38:51 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Centric_Consulting_Project_Team_9.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Centric_Consulting_Project_Team_9.Startup))]
+namespace Centric_Consulting_Project_Team_9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
